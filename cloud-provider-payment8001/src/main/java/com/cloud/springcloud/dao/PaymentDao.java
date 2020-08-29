bb7ba7e444c48f358cb07ad1270b36717202aac3
admin <admin>
2020-08-29 12:03:12 +0800

    完成前期准备工作

---
+++ cloud-provider-payment8001/src/main/java/com/cloud/springcloud/dao/PaymentDao.java
@@ -0,0 +1,24 @@
+package com.cloud.springcloud.dao;
+
+import com.cloud.springcloud.entities.Payment;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+/**
+ * @Description: payment的dao层
+ */
+@Mapper
+public interface PaymentDao {
+
+    /**
+     * 添加数据
+     * @Param [payment]
+     **/
+    public int create(Payment payment);
+
+    /**
+     * 根据id查询数据
+     * @Param [id]
+     **/
+    public Payment getPaymentById(@Param("id") Long id);
+}
---
+++ cloud-provider-payment8001/src/main/java/com/cloud/springcloud/entities/CommonResult.java
@@ -0,0 +1,23 @@
+package com.cloud.springcloud.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @Description: 前端返回结果类
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CommonResult<T> {
+
+    private Integer code;
+    private String message;
+    private T data;
+
+    //防止data为null设置两个参数的构造函数
+    public CommonResult(Integer code,String message){
+        this(code,message,null);
+    }
+}
