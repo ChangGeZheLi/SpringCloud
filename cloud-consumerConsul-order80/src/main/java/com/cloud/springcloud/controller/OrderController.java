f57967b6989b2f7bf328a37859aa2eb8b12c6f8c
admin <admin>
2020-08-31 16:30:05 +0800

    将provider和consumer注册到Consul注册中心，测试consumer调用provider

---
+++ cloud-consumerConsul-order80/src/main/java/com/cloud/springcloud/controller/OrderController.java
@@ -0,0 +1,29 @@
+package com.cloud.springcloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+
+/**
+ * @Description:
+ */
+@RestController
+@Slf4j
+public class OrderController {
+
+    //使用在consul中注册的服务名称访问
+    private static final String INVOKE_URL = "http://consul-provider-payment";
+
+    @Resource
+    private RestTemplate restTemplate;
+
+    @GetMapping("/consumer/payment/consul")
+    public String paymentInfo(){
+        String result = restTemplate.getForObject(INVOKE_URL + "/payment/consul", String.class);
+        return result;
+    }
+
+}
